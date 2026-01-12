6f915177cf8e574efe5a3e2dad96cdb742134a9a
nigelhayes11 <manus11@linuxmail.org>
2026-01-12 16:44:20 +0300

    Update birlestir.py

--- birlestir.py
+++ birlestir.py
@@ -6,6 +6,7 @@ inn = 'inn.m3u'
 selcuk = 'selcuk.m3u'
 an = 'an.m3u'
 kbl = 'kbl.m3u'
+gl = 'gl.m3u'
 cikis_dosyasi = 'man26.m3u'
 
 # M3U dosyalarının içeriğini oku
@@ -21,10 +22,11 @@ inn_icerik = oku_m3u(inn)
 selcuk_icerik = oku_m3u(selcuk)
 an_icerik = oku_m3u(an)
 kbl_icerik = oku_m3u(kbl)
+gl_icerik = oku_m3u(gl)
 
 
 # Birleştir
-birlesik_icerik = kbl_icerik + ftb_icerik + r_icerik + r2_icerik + inn_icerik + selcuk_icerik + an_icerik 
+birlesik_icerik = kbl_icerik + ftb_icerik + r_icerik + r2_icerik + inn_icerik + selcuk_icerik + an_icerik + gl_icerik 
 
 # Yeni dosyaya yaz
 with open(cikis_dosyasi, 'w', encoding='utf-8') as f:
