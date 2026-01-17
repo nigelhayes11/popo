02dab310ac9af3cfbbdbdaf2a2b743370ca3dc4e
nigelhayes11 <manus11@linuxmail.org>
2026-01-17 19:13:06 +0300

    Update fll.py

--- fll.py
+++ fll.py
@@ -8,6 +8,7 @@ an = 'an.m3u'
 kbl = 'kbl.m3u'
 ne = 'ne.m3u'
 rnl = 'rnl.m3u'
+liveeventsfilter = 'liveeventsfilter.m3u8'
 cikis_dosyasi = 'man26.m3u'
 
 # M3U dosyalarının içeriğini oku
@@ -25,10 +26,10 @@ an_icerik = oku_m3u(an)
 kbl_icerik = oku_m3u(kbl)
 ne_icerik = oku_m3u(ne)
 rnl_icerik = oku_m3u(rnl)
-
+liveeventsfilter_icerik = oku_m3u8(liveeventsfilter)
 
 # Birleştir
-birlesik_icerik = kbl_icerik + ftb_icerik + r_icerik + r2_icerik + inn_icerik + selcuk_icerik + an_icerik + ne_icerik + rnl_icerik 
+birlesik_icerik = kbl_icerik + ftb_icerik + r_icerik + r2_icerik + inn_icerik + selcuk_icerik + an_icerik + ne_icerik + rnl_icerik + liveeventsfilter_icerik  
 
 # Yeni dosyaya yaz
 with open(cikis_dosyasi, 'w', encoding='utf-8') as f:
